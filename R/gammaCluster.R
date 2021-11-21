458c6ee4795b39c1ad5b6886fc481bb0b826077c
aarkatkar <51249468+aarkatkar@users.noreply.github.com>
2021-11-20 21:53:48 -0500

    Refined Probabilistic Methods

--- R/gammaCluster.R
+++ R/gammaCluster.R
@@ -1,105 +1,135 @@
-gammaCluster <- function(gm, n, maxiter=200){
-  initSeeds <- .initialSeeds(gm, n)
-  m <- initSeeds$membership
-  A <- initSeeds$A
-  D <- initSeeds$D
-  m <- .initialMembership(m, D, n)
-  opGamma <- .optimizeGamma(m, A, maxiter)
+gammaCluster <- function(gm, m, maxiter = 200) {
+    A <- -log(gm)
+    diag(A) <- 0
+    opGamma <- .optimizeGamma(m, A, maxiter)
 
-  return(list(labels=opGamma$clusters, membership=opGamma$membership))
+    return(list(
+        labels = opGamma$clusters,
+        membership = opGamma$membership
+    ))
 }
 
-.initialSeeds <- function(gm, n){
-  A <- -log(gm)
-  diag(A) <- 0
-  D <- -log1p(-gm)
-  diag(D) <- 0
-  N <- dim(A)[1]
-  bld <- cluster::pam(D, n, diss=TRUE, do.swap=FALSE)
-  selected <- bld$medoids
 
-  m <- matrix(rep(0, N*n), nrow=n)
-  for (i in seq_len(n)){
-    m[i,selected[i]]=1
-  }
-  return(list(membership=m, A=A, D=D))
-}
-
-.optimizeGamma <- function(m, A, maxiter){
-  clusters <- Rfast::colMaxs(m, value=FALSE)
-  converged <- 0
-  for (i in 1:maxiter){
-    d <- eigenMapMatMult(m, A)
-    total <- Rfast::rowsums(m)-m
-    p <- -pgamma(d, total, lower.tail=FALSE, log.p=TRUE)
-    maxCol <- Rfast::colMaxs(p, parallel=TRUE)
-    upper <- t(exp(t(p)-maxCol))
-    total <- Rfast::colsums(upper, parallel=TRUE)
-    m <- t(t(upper)/total)
-    clusters0 <- Rfast::colMaxs(m, value=FALSE)
-    if (Rfast::all_equals(clusters0, clusters, fast_result=TRUE) & i > 10){
-      converged <- 1
-      break
+.optimizeGamma <- function(m, A, maxiter) {
+    clusters <- Rfast::colMaxs(m, value = FALSE)
+    converged <- 0
+    for (i in 1:maxiter) {
+        d <- eigenMapMatMult(m, A)
+        total <- Rfast::rowsums(m) - m
+        p <- -pgamma(d, total, lower.tail = FALSE, log.p = TRUE)
+        maxCol <- Rfast::colMaxs(p, parallel = TRUE)
+        upper <- t(exp(t(p) - maxCol))
+        total <- Rfast::colsums(upper, parallel = TRUE)
+        m <- t(t(upper) / total)
+        clusters0 <- Rfast::colMaxs(m, value = FALSE)
+        if (Rfast::all_equals(clusters0, clusters, fast_result = TRUE) &
+            i > 10) {
+            converged <- 1
+            break
+        }
+        clusters <- clusters0
     }
-    clusters <- clusters0
-  }
-  return(list(clusters=clusters, membership=m, converged=converged))
+    return(list(
+        clusters = clusters,
+        membership = m,
+        converged = converged
+    ))
 }
 
-.initialMembership <- function(m, D, n){
-  d <- -n*eigenMapMatMult(m, D)
-  upper <- exp(d-max(d))
-  total <- rowSums(t(upper))
-  m <- t(t(upper)/total)
-  return(m)
+gammaPlot <- function(gm, clusters) {
+    colorList <- scales::hue_pal()(max(clusters))
+    u <- as.data.frame(umap::umap(-log1p(-gm), input = "dist")$layout)
+    colnames(u) <- c("UMAP1", "UMAP2")
+    u$clusters <- as.factor(clusters)
+    ggplot2::ggplot() + ggplot2::coord_cartesian() +
+        ggplot2::scale_x_continuous() +
+        ggplot2::scale_y_continuous() +
+        ggplot2::scale_color_hue() +
+        ggplot2::layer(
+            data = u,
+            mapping = ggplot2::aes(x = UMAP1,
+                                   y = UMAP2,
+                                   color = clusters),
+            stat = "identity",
+            geom = "point",
+            position = ggplot2::position_jitter()
+        ) +
+        ggplot2::theme_classic()
 }
 
-gammaPlot <- function(gm, clusters){
-  colorList <- scales::hue_pal()(max(clusters))
-  u <- as.data.frame(umap::umap(-log1p(-gm), input="dist")$layout)
-  colnames(u) <- c("UMAP1", "UMAP2")
-  u$clusters <- as.factor(clusters)
-  ggplot2::ggplot() + ggplot2::coord_cartesian() +
-    ggplot2::scale_x_continuous() +
-    ggplot2::scale_y_continuous() +
-    ggplot2::scale_color_hue() +
-    ggplot2::layer(data=u,
-                   mapping=ggplot2::aes(x=UMAP1, y=UMAP2, color=clusters),
-                   stat="identity",
-                   geom="point",
-                   position=ggplot2::position_jitter()
-    )
+gammaTest <- function(x, gm, rbound, maxiter = 200, plot.show=TRUE, excludeFirst=FALSE) {
+    A <- -log(gm)
+    diag(A) <- 0
+
+    if(excludeFirst){
+        n <- c(3:rbound)
+    }else{
+        n <- c(2:rbound)
+    }
+    clustScores <- vapply(n,
+                          function(n_)
+                              .gammaEval(x, A, n_, maxiter, excludeFirst),
+                          numeric(2))
+    scores <- clustScores[1,]
+    convergence <- clustScores[2,]
+    pch <- ifelse(convergence == 0, 4,
+                  ifelse(scores == max(scores), 8, 16))
+    p.color <- ifelse(convergence == 0,
+                      "red",
+                      ifelse(scores == max(scores), "blue", "black"))
+    if(plot.show){
+        plot(
+            n,
+            scores,
+            xlab = "number of clusters",
+            ylab = "log-likelihood",
+            pch = pch,
+            col = p.color
+        )
+    }
+    return(scores)
 }
 
-gammaTest <- function(start, end, gm, maxiter=200){
-  initSeeds <- .initialSeeds(gm, end)
-  m <- initSeeds$membership
-  A <- initSeeds$A
-  D <- initSeeds$D
+.gammaEval <- function(x, A, n, maxiter, excludeFirst) {
+    m <- gelSVD(x, n, excludeFirst)
 
-  n <- c(start:end)
-  clustScores <- vapply(n,
-                        function(n_) .gammaEval(m, A, D, n_, maxiter),
-                        numeric(2))
-  scores <- clustScores[1,]
-  convergence <- clustScores[2,]
-  pch <- ifelse(convergence==0,4,
-                ifelse(scores==min(scores), 8, 16))
-  p.color <- ifelse(convergence==0,"red",
-                    ifelse(scores==min(scores), "blue", "black"))
-  plot(n, scores, xlab="n", ylab="Modified Partition Entropy",
-       pch=pch, col=p.color)
-
+    opGamma <- .optimizeGamma(m, A, maxiter)
+    m <- opGamma$membership
+    converged <- opGamma$converged
+    mtot <- log(colSums(m^2))
+    mpe<- sum(mtot)
+    return(c(mpe, converged))
 }
 
-.gammaEval <- function(m, A, D, n, maxiter){
-  m <- .initialMembership(m[seq_len(n),], D, n)
-  opGamma <- .optimizeGamma(m, A, maxiter)
-  m <- opGamma$membership
-  converged <- opGamma$converged
-  N <- dim(A)[1]
-  partEnt <- -1/N*sum(ifelse(m==0, 0, m*log(m)))
-  mpe <- N*partEnt/(N-n)
-  return(c(mpe, converged))
+subClusters <- function(x, clust, rbound, squared=FALSE, z=NULL, maxiter=200){
+    n <- seq_len(dim(clust$membership)[1])
+    eg <- eigenGenes(x, clust$membership)
+    clust.labels <- vapply(n, function(n_)
+        .subCluster(n_,x, clust, eg, rbound, squared, z, maxiter),
+        numeric(dim(x)[1]))
+    combined <- rowSums(clust.labels)
+    mapping <- floor(sort(unique(combined))/rbound)+1
+    return(list(
+        labels=data.table::frank(combined, ties.method="dense"),
+        roots=mapping
+    ))
+}
+
+.subCluster <- function(n, x, clust, eg, rbound, squared, z, maxiter){
+    x.blank <- rep(0, dim(x)[1])
+    if(sum(clust$labels==n)==1){
+        x.blank[clust$labels==n] <- (n-1)*rbound+1
+    }else if(sum(clust$labels==n)==2){
+        x.blank[clust$labels==n] <- (n-1)*rbound + c(1,2)
+    }else if(sum(clust$labels==n)!=0){
+        x.sub <- x[clust$labels==n,]
+        gm <- gelMatrix(x.sub, z=rbind(z, eg[n,]), squared=squared)
+        scores <- gammaTest(x.sub, gm, rbound, maxiter, plot.show=FALSE, excludeFirst=TRUE)
+        n.optimal <- which.max(scores)+2
+        me <- gelSVD(x.sub, n.optimal, excludeFirst=TRUE)
+        subclusts <- gammaCluster(gm, me, maxiter)
+        x.blank[clust$labels==n] <-(n-1)*rbound + subclusts$labels
+    }
+    return(x.blank)
 }
 
--- R/gelFuncs.R
+++ R/gelFuncs.R
@@ -1,48 +1,55 @@
-eigenGenes <- function(x, m){
-  xm <- apply(x, 1, mean)
-  xsd <- apply(x, 1, sd)
-  xt <- (x - xm)/xsd
-  egenes <- t(apply(m, 1, function(mi) .getEigen(xt, mi)))
+eigenGenes <- function(x, m) {
+    xm <- apply(x, 1, mean)
+    xsd <- apply(x, 1, sd)
+    xt <- (x - xm) / xsd
+    egenes <- t(apply(m, 1, function(mi) {
+          .getEigen(xt, mi)
+      }))
 }
 
-.getEigen <- function(xt, m){
-  if (sum(m) == 0){
-    egene <- rep(0, dim(xt)[2])
-  }else{
-    egene <- prcomp(xt*m)$rotation[,1]
-    weightMean <- apply(xt, 2, function(b){weighted.mean(b, m)})
-    if (cor(egene, weightMean)<0){
-      egene <- -1*egene
+.getEigen <- function(xt, m) {
+    if (sum(m) == 0) {
+        egene <- rep(0, dim(xt)[2])
+    } else {
+        egene <- prcomp(xt * m)$rotation[, 1]
+        weightMean <- apply(xt, 2, function(b) {
+            weighted.mean(b, m)
+        })
+        if (cor(egene, weightMean) < 0) {
+            egene <- -1 * egene
+        }
     }
-  }
-  return(egene)
+    return(egene)
 }
 
-gelDiff <- function(gm1, gm2, twoTailed=FALSE, unifOut=FALSE){
-  gmDiff <- log(gm2)-log(gm1)
-  diag(gmDiff) <- 0
-  p <- 0.5*exp(-abs(gmDiff))
-  if (twoTailed){
-    pval <- 2 * p
-  }else{
-    pval <- ifelse(gmDiff>0, p, 1-p)
-  }
-  N <- dim(gm2)[1]
-  if (unifOut){
-    pval <- .forceUniform(pval, lower.tail=TRUE)
-  }
-  return(pval)
+gelDiff <- function(gm1,
+                    gm2,
+                    twoTailed = FALSE,
+                    unifOut = FALSE) {
+    gmDiff <- log(gm2) - log(gm1)
+    diag(gmDiff) <- 0
+    p <- 0.5 * exp(-abs(gmDiff))
+    if (twoTailed) {
+        pval <- 2 * p
+    } else {
+        pval <- ifelse(gmDiff > 0, p, 1 - p)
+    }
+    N <- dim(gm2)[1]
+    if (unifOut) {
+        pval <- .forceUniform(pval, lower.tail = TRUE)
+    }
+    return(pval)
 }
 
-.forceUniform <- function(x, lower.tail=FALSE){
-  vals <- x[upper.tri(x)]
-  N_ <- length(vals)
-  if (lower.tail){
-    rankMat <- data.table::frank(vals)/(N_+1)
-  }else{
-    rankMat <- (N_ + 1 - data.table::frank(vals))/(N_+1)
-  }
-  p <- diag(0, nrow=dim(x)[1])
-  p[upper.tri(p)] <- rankMat
-  return(p + t(p))
+.forceUniform <- function(x, lower.tail = FALSE) {
+    vals <- x[upper.tri(x)]
+    N_ <- length(vals)
+    if (lower.tail) {
+        rankMat <- data.table::frank(vals) / (N_ + 1)
+    } else {
+        rankMat <- (N_ + 1 - data.table::frank(vals)) / (N_ + 1)
+    }
+    p <- diag(0, nrow = dim(x)[1])
+    p[upper.tri(p)] <- rankMat
+    return(p + t(p))
 }
